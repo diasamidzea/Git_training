58181eac8239ad7945eab31887d3616a98e5dcb0
Alexey Diasamidze <diasamidze@bk.ru>
2019-01-06 12:30:10 +0300

    1

---
+++ Diary.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <div>
+
+    <h1>Ежедневник</h1>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "diary"
+    }
+</script>
+
+<style scoped>
+
+</style>
